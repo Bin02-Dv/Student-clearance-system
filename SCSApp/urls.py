75947d1a3f751456d7056f500769377256eba56d
Bin02-Dv <alaminbinahmad78@gmail.com>
2024-05-07 14:49:51 +0100

    5/7/2024 - 2:49pm

--- SCSApp/urls.py
+++ SCSApp/urls.py
@@ -6,6 +6,13 @@ urlpatterns = [
     path('sign-up/', views.sign_up, name='sign-up'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('user-management/', views.user_management, name='user-management'),
+    path('add-user/', views.add_user, name='add-user'),
+    path('user-record/', views.user_record, name='user-record'),
     path('student-management/', views.student_management, name='student-management'),
     path('student-record/', views.student_record, name='student-record'),
+    path('clearance/', views.clearance, name='clearance'),
+    path('student-clearance/', views.student_clearance, name='student-clearance'),
+    path('cleared-student/', views.cleared_student, name='cleared-student'),
+    path('uncleared-student/', views.uncleared_student, name='uncleared-student'),
+    path('change-password/', views.change_password, name='change-password'),
 ]
--- SCSApp/views.py
+++ SCSApp/views.py
@@ -15,8 +15,29 @@ def dashboard(request):
 def user_management(request):
     return render(request, 'dashboard/user-management.html')
 
+def add_user(request):
+    return render(request, 'dashboard/add-user.html')
+
+def user_record(request):
+    return render(request, 'dashboard/user-record.html')
+
 def student_management(request):
     return render(request, 'dashboard/student-management.html')
 
 def student_record(request):
     return render(request, 'dashboard/student-record.html')
+
+def cleared_student(request):
+    return render(request, 'dashboard/cleared-student.html')
+
+def uncleared_student(request):
+    return render(request, 'dashboard/uncleared-student.html')
+
+def clearance(request):
+    return render(request, 'dashboard/clearance.html')
+
+def student_clearance(request):
+    return render(request, 'dashboard/student-clearance.html')
+
+def change_password(request):
+    return render(request, 'dashboard/change-password.html')
